7b684b3d164cda7407cce27d7fb81348bb3d8893
mamo1016 <rr0061vv@ed.ritsumei.ac.jp>
2018-02-05 16:49:17 +0900

    first commit

--- count/ViewController.swift
+++ count/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var label: UILabel!
+    var number: Double = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+//        color()
+        backGround()
+    
+    }
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+//        color()
+        backGround()
+    }
 
+    func color(){
+        if number <= -10 {
+            label.textColor = UIColor.red
+        }else if number >= 10{
+            label.textColor = UIColor.blue
+        }
+    }
+    
+    func backGround(){
+        self.view.backgroundColor = UIColor(red: CGFloat(number*0.1), green: CGFloat(number*0.01), blue: CGFloat(number*0.001), alpha: 1.0)
+    }
 }
 
